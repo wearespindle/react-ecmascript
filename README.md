9661c1482918c3833681732a8ce5995ddfef128e
Joël Kuijten <joel@pm5544.eu>
2018-07-31 13:47:00 +0200

    adds explanation about the new ways of interacting with this script to the README.md

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 # react-ecmascript
-In the root of this repo are four scripts which are transformed versions of the UMD distribution versions of react and react-dom (both development and production).
+In the root of this repo are four ECMAScript versions of the UMD distribution versions of react and react-dom (both development and production).
 
-A script is used to transforms React and ReactDOM into ECMAScript versions so they can be used in browsers supporting ECMAScript modules and module loading.
+A script is used to transforms react and react-dom into ECMAScript versions so they can be used in browsers supporting ECMAScript modules and module loading.
 If you want to generate them again run ```$ npm i``` and ```$ npm run build```
 
-# usage
-To use these scripts you need to copy these files over to a place where they can be downloaded by a browser (probably a build script or a development script), and the ```'react'``` in ```import react from 'react';``` need to be changed to the uri through which they can be downloaded.
+If you want to have this script replace the import source in the react-dom modules for you you can pass in the --uri (or -u) parameter like this ```npm run build -- --uri https://some-uri.com/vendor/``` (mention the extra --).
+When calling this script directly you can use ```node src/index.js --uri https://some-other-uri.com/``` or ```node src/index.js -u /some/dir/```
 
-You can automate that in your project by for instance using Rollup.
+You can also use this as a module if you want to get the sources returned as a Promise which resolves to an object containing all four the modules as strings. Like so:
+```const reactEcmascript = require('react-ecmascript');
+reactEcmascript('someUrl') // optional uri as argument
+  .then(sources => {
+    // sources is an object with the properties being the filename (like react.production.min.mjs) and the properties being the content of that module as a string
+
+    // do something with these sources, like saving them to disk or log them
+    console.log(Object.keys(sources));
+  });```
+
+To use the exported ECMAScript modules in a browser you copy these files over to a place where they can be downloaded by a browser (probably a build script or a development script), and the ```'react'``` in ```import react from 'react';``` need to be changed to the uri through which they can be downloaded. Like mentioned above this can be done by passing in the uri.
+
+You can automate that in your project by for instance using Rollup. For instance by adding the sources in the root to a Rollup configs ```external``` and ```output.paths```.
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const { join, extname } = require('path');
 const { promisify } = require('util');
 const recast = require('recast');
 const parser = require('recast/parsers/acorn');
 
-const makeDir = require('make-dir');
+const args = require('args').option(
+  'uri',
+  `The URI of the browser reachable directory the scripts will be served from.
+The relevant filename will be added to load the correct version (production.min or development).
+Examples: https://localhost:3000/static/ or /vendor/ or https://www.my-site.com/static/vendor/`
+);
+const flags = args.parse(process.argv);
+
 const walk = require('./walk');
 
 const read = promisify(fs.readFile);
-const readDir = promisify(fs.readdir);
+const readDirectory = promisify(fs.readdir);
 const write = promisify(fs.writeFile);
 
 const baseDirectory = [__dirname];
@@ -16,34 +25,71 @@ const transformersDirectory = baseDirectory.concat('transformers');
 const targetsDirectory = baseDirectory.concat('targets');
 const joiner = p => join.apply(null, p);
 
-readDir(joiner(transformersDirectory)).then(filenames =>
-  Promise.all(
-    filenames.map(filename => {
-      const { targetSelectSteps, sourceFile, sourceSelectSteps } = require(joiner(
-        transformersDirectory.concat(filename)
-      ));
+const shouldWriteLibrariesToDisk = require.main === module;
 
-      return read(joiner(targetsDirectory.concat(filename)), 'utf8').then(targetContent => {
-        const targtetAst = recast.parse(targetContent, { parser });
+function getModuleStrings(importSourceUri = flags.uri) {
+  return readDirectory(joiner(transformersDirectory))
+    .then(filenames =>
+      Promise.all(
+        filenames.map(filename => {
+          const {
+            targetSelectSteps,
+            sourceFile,
+            sourceSelectSteps,
+            importSelectSteps,
+            importFilename
+          } = require(joiner(transformersDirectory.concat(filename)));
 
-        return read(sourceFile).then(sourceContent => {
-          const sourceAst = recast.parse(sourceContent, { parser });
+          return read(joiner(targetsDirectory.concat(filename)), 'utf8').then(targetContent => {
+            const targtetAst = recast.parse(targetContent, { parser });
 
-          // reference the part in the source AST we want to use
-          const body = walk(sourceAst.program, sourceSelectSteps);
+            return read(sourceFile).then(sourceContent => {
+              const newFilename = filename.replace(extname(filename), '.mjs');
 
-          // select the part we want to replace in the target with the part of the source AST
-          walk(targtetAst.program, targetSelectSteps).body = body;
+              const sourceAst = recast.parse(sourceContent, { parser });
 
-          const generated = recast.print(targtetAst, { parser }).code;
+              // reference the part in the source AST we want to use
+              const body = walk(sourceAst.program, sourceSelectSteps);
 
-          const newFilename = filename.replace(extname(filename), '.mjs');
+              // select the part we want to replace in the target with the part of the source AST
+              walk(targtetAst.program, targetSelectSteps).body = body;
 
-          return write(join(__dirname, '..', newFilename), generated, 'utf8').then(() =>
-            console.log(`written /${newFilename}`)
-          );
-        });
-      });
-    })
-  )
-);
+              if (importSourceUri && importSelectSteps && importFilename) {
+                const finalImpoprtSourceUri = `${importSourceUri}${importFilename}`;
+                walk(targtetAst.program, importSelectSteps).value = finalImpoprtSourceUri;
+                console.log(`replaced import source in ${newFilename} to ${finalImpoprtSourceUri}`);
+              }
+
+              const generated = recast.print(targtetAst, { parser }).code;
+
+              return {
+                filename: newFilename,
+                content: generated
+              };
+            });
+          });
+        })
+      )
+    )
+    .then(generated =>
+      generated.reduce(
+        (prev, { filename, content }) => Object.assign(prev, { [filename]: content }),
+        {}
+      )
+    );
+}
+
+if (shouldWriteLibrariesToDisk) {
+  getModuleStrings().then(generated =>
+    Object.keys(generated).map(filename =>
+      write(join(__dirname, '..', filename), generated[filename], 'utf8').then(() =>
+        console.log(`written /${filename}`)
+      )
+    )
+  );
+}
+
+// export the function to enable external scripts to call this module and get an object returned with the ECMAScript modules as strings
+module.exports = function(uri) {
+  return getModuleStrings(uri);
+};
